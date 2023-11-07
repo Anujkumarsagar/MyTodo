83301990d738bd4e9281abc991df26769e56d843
Anujkumarsagar <109503594+Anujkumarsagar@users.noreply.github.com>
2023-11-07 13:27:45 +0530

    todo.py
    This is a simple to-do list application created using Tkinter, a popular GUI library for Python. Here's a brief description of the key features and components of the application:
    
    1. **Window Title and Icon**:
       - The application window is titled "MyTodo."
       - It has an icon set using the `root.iconphoto` method.
    
    2. **Background Image**:
       - The application window features a background image (image_bg.png) that is displayed using a `Label` widget.
    
    3. **Task Entry**:
       - Users can enter their tasks into the "Task" entry field.
       - The entry field is styled with a specific font and background color.
    
    4. **Task List**:
       - Entered tasks are displayed in a `Listbox` widget, allowing users to view and manage their to-do list.
       - The `Listbox` is styled with a specific height, width, and background color.
    
    5. **Add Task Button**:
       - There is an "Add" button, which, when clicked, adds the entered task to the task list displayed in the `Listbox`.
    
    6. **Delete Task Button**:
       - The "Delete" button, when clicked, deletes the selected task(s) from the task list.
    
    7. **Background Image for the Frame**:
       - A frame with a raised border and a background image is used to contain the task-related widgets.
       - The frame is set to expand with the window.
    
    8. **Global Task Number**:
       - The application keeps track of the task number to display tasks with sequential numbering.
    
    9. **Color Styling**:
       - Various widgets are styled with different fonts, colors, and background colors to enhance the application's visual appeal.
    
    The application provides a simple interface for adding and managing tasks. Users can enter tasks in the input field, click "Add" to add them to the list, and click "Delete" to remove selected tasks. The background image and color choices make the application visually appealing.
    
    Please note that you should ensure the image file paths (`image_bg.png` and `logo.png`) are correctly specified according to your system's file structure.

---
+++ todo.py
@@ -0,0 +1,65 @@
+
+import tkinter as tk
+
+from tkinter import Scrollbar
+from PIL import ImageTk , Image
+
+# Create the tkinter root window
+root = tk.Tk()
+img = Image.open("E:\python programming\projects for codesoft\projects for codesoft\logo.png")
+logo_img = img.resize((16, 16))
+logo = ImageTk.PhotoImage(image=logo_img)
+root.iconphoto(False, logo)
+root.title("MyTodo")
+
+
+
+def add_task():
+    global task_number  # Use t
+    task = input_task.get()
+    if task :
+        task_text.insert(tk.END,str(task_number)+ ":" + task )
+        input_task.delete(0,tk.END)
+        task_number += 1
+def deleted_selected_task():
+    selected = task_text.curselection()
+    if selected:
+        task_text.delete(selected)
+
+
+# Create a frame to hold the background image
+
+original_image = Image.open("E:\python programming\projects for codesoft\image_bg.png")
+new_w = 700
+new_h = 400
+resize_image = original_image.resize((new_w,new_h))
+image = ImageTk.PhotoImage(image=resize_image)
+frame = tk.Frame(root , relief="raised", borderwidth=2,)
+# Configure the frame for expansion
+frame.pack(fill="both", expand=True)
+
+label = tk.Label(frame, image=image)
+label.pack()
+
+Task = tk.Label(label, text="Task :", font=("Arial", 15), bg="#ca81d3")
+Task.place(x=200, y=25)
+
+
+input_task = tk.Entry(label, width=15, font=("Comic Sans MS", 15, "bold"),bg="#dcbaf0")
+input_task.place(x=260, y=20)
+
+
+task_text = tk.Listbox(label,height=15, width=34,bg="#d29fea")
+task_text.place(x=247, y=78)
+# Task.config(bg='systemTransparent')
+
+button1 = tk.Button(label, text="Delete", font=("arial", 12),cursor="hand2",border=0.2,command=deleted_selected_task,bg="#e69aa6",activebackground="#974ce1")
+button1.place(x=290, y=350)
+
+button1 = tk.Button(label, text="Add", font=("arial", 12),cursor="hand2",border=0.2,command=add_task,bg="#e69aa6",activebackground="#974ce1")
+button1.place(x=370, y=350)
+
+task_number = 1  # Initialize the task number
+
+# Run the tkinter main loop to display the window
+root.mainloop()
